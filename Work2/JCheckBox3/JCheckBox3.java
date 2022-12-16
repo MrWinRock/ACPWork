5c09585d8cc389be41eb79321ebfd85c420355b6
MrWinRock <mrwinrock11@gmail.com>
2022-12-16 22:08:20 +0700

    edit JC3 & add JC2

--- Work2/JCheckBox3/JCheckBox3.java
+++ Work2/JCheckBox3/JCheckBox3.java
@@ -4,68 +4,68 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class JCheckBox3 extends JFrame implements ActionListener {
-// Prices of the menu items
-private static final double PIZZA_PRICE = 10;
-private static final double BURGER_PRICE = 5;
-private static final double WATER_PRICE = 1;
+    // Prices of the menu items
+    private static final double PIZZA_PRICE = 10;
+    private static final double BURGER_PRICE = 5;
+    private static final double WATER_PRICE = 1;
 
-// Checkboxes for the menu items
-private JCheckBox pizzaCheckBox;
-private JCheckBox burgerCheckBox;
-private JCheckBox waterchecbox;
+    // Checkboxes for the menu items
+    private JCheckBox pizzaCheckBox;
+    private JCheckBox burgerCheckBox;
+    private JCheckBox waterchecbox;
 
-// Label to display the total cost
-private JLabel totalCostLabel;
+    // Label to display the total cost
+    private JLabel totalCostLabel;
 
-public JCheckBox3() {
-    // Set the title and layout of the window
-    setTitle("Food Ordering System");
-    setLayout(new GridLayout(4, 1));
+    public JCheckBox3() {
+        // Set the title and layout of the window
+        setTitle("Food Ordering System");
+        setLayout(new GridLayout(4, 1));
 
-    // Create the checkboxes & add checkboxes
-    pizzaCheckBox = new JCheckBox("Pizza ($" + PIZZA_PRICE + ")");
-    burgerCheckBox = new JCheckBox("Burgers ($" + BURGER_PRICE + ")");
-    waterchecbox = new JCheckBox("Water ($" + WATER_PRICE + ")");
-    add(pizzaCheckBox);
-    add(burgerCheckBox);
-    add(waterchecbox);
+        // Create the checkboxes & add checkboxes
+        pizzaCheckBox = new JCheckBox("Pizza ($" + PIZZA_PRICE + ")");
+        burgerCheckBox = new JCheckBox("Burgers ($" + BURGER_PRICE + ")");
+        waterchecbox = new JCheckBox("Water ($" + WATER_PRICE + ")");
+        add(pizzaCheckBox);
+        add(burgerCheckBox);
+        add(waterchecbox);
 
-    // Create a panel
-    JPanel totalCostPanel = new JPanel();
-    totalCostLabel = new JLabel("Total cost: $0");
-    totalCostPanel.add(totalCostLabel);
-    add(totalCostPanel);
+        // Create a panel
+        JPanel totalCostPanel = new JPanel();
+        totalCostLabel = new JLabel("Total cost: $0");
+        totalCostPanel.add(totalCostLabel);
+        add(totalCostPanel);
 
-    // Add action listeners
-    pizzaCheckBox.addActionListener(this);
-    burgerCheckBox.addActionListener(this);
-    waterchecbox.addActionListener(this);
-}
-
-@Override
-public void actionPerformed(ActionEvent e) {
-    
-    double totalCost = 0;
-    if (pizzaCheckBox.isSelected()) {
-    totalCost += PIZZA_PRICE;
-    }
-    if (burgerCheckBox.isSelected()) {
-    totalCost += BURGER_PRICE;
-    }
-    if (waterchecbox.isSelected()) {
-    totalCost += WATER_PRICE;
+        // Add action listeners
+        pizzaCheckBox.addActionListener(this);
+        burgerCheckBox.addActionListener(this);
+        waterchecbox.addActionListener(this);
     }
 
-    // Update the total cost label
-    totalCostLabel.setText("Total cost: $" + totalCost);
-}
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-public static void main(String[] args) {
+        double totalCost = 0;
+        if (pizzaCheckBox.isSelected()) {
+        totalCost += PIZZA_PRICE;
+        }
+        if (burgerCheckBox.isSelected()) {
+        totalCost += BURGER_PRICE;
+        }
+        if (waterchecbox.isSelected()) {
+        totalCost += WATER_PRICE;
+        }
 
-    JCheckBox3 window = new JCheckBox3();
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.pack();
-    window.setSize(300, 300);
-    window.setVisible(true);
-}
+        // Update the total cost label
+        totalCostLabel.setText("Total cost: $" + totalCost);
+    }
+
+    public static void main(String[] args) {
+
+        JCheckBox3 window = new JCheckBox3();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setSize(300, 300);
+        window.setVisible(true);
+    }
 }
